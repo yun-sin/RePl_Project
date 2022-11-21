b8f3edd73abbd8dd51147c9aeb8e82ceeb34e2b2
Jang Yunsin <99275134+yun-sin@users.noreply.github.com>
2022-11-21 15:01:14 +0900

    경품페이지 메인 경품 컨텐츠 슬라이드,  서브 경품 컨텐츠 #46

---
+++ src/components/raffle/SubContent.js
@@ -0,0 +1,47 @@
+import React, { memo, useState } from "react";
+import styled from "styled-components";
+import { data } from "./data";
+import img0 from "./Images/tumbler.jpg";
+
+const SubContentContainer = styled.div`
+  width: 80%;
+  max-width: 1000px;
+  margin: auto;
+  margin-top: 10vh;
+  text-align: center;
+
+  img {
+    width: 100%;
+  }
+
+  .content {
+    display: inline-block;
+    width: 25%;
+    padding: 10px;
+    box-sizing: border-box;
+
+    background-color: #ddd;
+    margin-bottom: 1vw;
+  }
+`;
+
+const SubContent = memo(() => {
+  return (
+    <SubContentContainer>
+      {data.map((v, i) => {
+        return (
+          <div key={i} className="content">
+            <img src={img0} />
+            <div>
+              <span>{v.price}</span>
+              <br />
+              <span>{` ${v.remaining} /${v.total}`}</span>
+            </div>
+          </div>
+        );
+      })}
+    </SubContentContainer>
+  );
+});
+
+export default SubContent;
