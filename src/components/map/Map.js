c48322ed521fdef5562639dfa0ff5abe4d77f155
Jang Yunsin <99275134+yun-sin@users.noreply.github.com>
2022-12-06 06:55:25 +0900

    장소 상세정보 모달창 #17

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,57 +1,19 @@
 /*global kakao*/
 import React, { memo, useEffect, useRef, useState, useCallback } from "react";
-import styled from "styled-components";
 import { useSelector, useDispatch } from "react-redux";
-
 import { getMapData } from "../../slices/MapSlice";
-// import { positions } from "./data";
+import { MapContainer, ListContainer, SearchLoc, ModalContainer } from "./MapStyled";
+import Modal from "react-modal";
+import Spinner from "./Spinner";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLocationCrosshairs } from "@fortawesome/free-solid-svg-icons";
-import { faMagnifyingGlassLocation } from "@fortawesome/free-solid-svg-icons";
+import { faLocationCrosshairs, faMagnifyingGlassLocation, faX } from "@fortawesome/free-solid-svg-icons";
 
 import markerStar from "../../assets/img/map/markerStar.png";
 import markerRed from "../../assets/img/map/markerRed.png";
 import markerBlue from "../../assets/img/map/markerBlue.png";
-
-import { MapContainer } from "./MapContainer";
-import { ListContainer } from "./ListContainer";
-import Spinner from "./Spinner";
-
-const SearchLoc = styled.div`
-  width: 200px;
-  height: 50px;
-  border-radius: 10px;
-  background-color: #da4c1f;
-  z-index: 1;
-  position: fixed;
-  line-height: 1.8;
-  font-weight: bold;
-  color: white;
-  bottom: 4vw;
-  left: 50%;
-  transform: translate(-50%, 0);
-  opacity: 0.8;
-  padding: 10px;
-  box-sizing: border-box;
-  text-align: center;
-  cursor: pointer;
-  display: flex;
-
-  &:hover {
-    opacity: 1;
-  }
-
-  span {
-    width: 80%;
-  }
-
-  input,
-  svg {
-    width: 20%;
-    margin: 6.5px 0;
-  }
-`;
+import iconMore from "../../assets/img/map/icon-more.svg";
+import iconPlus from "../../assets/img/map/icon-plus-grey-sm.svg";
 
 const Map = memo(() => {
   const dispatch = useDispatch();
@@ -59,49 +21,51 @@ const Map = memo(() => {
 
   const yourLoc = useRef();
   const kakaoRef = useRef();
-  const [kakaoMap, setKakaoMap] = useState();
+  const [replMap, setReplMap] = useState();
   const [btnActive, setBtnActive] = useState();
 
-  useEffect(() => {
-    dispatch(getMapData());
-  }, []);
+  const [swLimit, setSwLimit] = useState();
+  const [neLimit, setNeLimit] = useState();
+  const [centerCoord, setCenterCoord] = useState([37.5025506249856, 127.02485228946493]);
+
+  const [modalContent, setModalContent] = useState(0);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   useEffect(() => {
     const container = document.getElementById("map");
     const options = {
       // 이젠 아카데미 위도 경도
-      center: new kakao.maps.LatLng(37.5025506249856, 127.02485228946493),
+      center: new kakao.maps.LatLng(centerCoord[0], centerCoord[1]),
       level: 3,
     };
     const map = new kakao.maps.Map(container, options);
-    setKakaoMap(map);
+    setReplMap(map);
     console.log("🗺️ 지도 렌더링");
 
+    dispatch(getMapData()).then((result) => console.log(result));
+  }, []);
+
+  useEffect(() => {
     if (data) {
+      console.log("📍 마커 렌더링");
       /**
        * 데이터에 저장된 위치 지도에 마커 출력
        */
+      const imageSize = new kakao.maps.Size(24, 35); // 마커 이미지의 이미지 크기 입니다
+      const overSize = new kakao.maps.Size(29, 42); // 마커 이미지의 이미지 크기 입니다
+
+      const markerImage = new kakao.maps.MarkerImage(markerBlue, imageSize); // 마커 이미지를 생성합니다
+      const overImage = new kakao.maps.MarkerImage(markerStar, overSize); // 확대된 마커 이미지를 생성합니다
+
       data.forEach((v, i) => {
-        var imageSize = new kakao.maps.Size(24, 35); // 마커 이미지의 이미지 크기 입니다
-        var overSize = new kakao.maps.Size(29, 42); // 마커 이미지의 이미지 크기 입니다
-
-        var markerImage = new kakao.maps.MarkerImage(markerBlue, imageSize); // 마커 이미지를 생성합니다
-        var overImage = new kakao.maps.MarkerImage(markerStar, overSize); // 확대된 마커 이미지를 생성합니다
-
-        // var geocoder = new kakao.maps.services.Geocoder();
-
         // 마커를 생성합니다
         const marker = new kakao.maps.Marker({
-          map: map, // 마커를 표시할 지도
+          map: replMap, // 마커를 표시할 지도
           position: new kakao.maps.LatLng(v.lat, v.lng), // 마커를 표시할 위치
           title: v.title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
           image: markerImage, // 마커 이미지
         });
-        marker.setMap(map);
-
-        // const el = document.createElement("div", "hi 123");
-        // el.innerHTML = "123";
-        // console.log(container);
+        marker.setMap(replMap);
 
         /**
          * 마커에 mouseover, mouseout 이벤트
@@ -118,7 +82,7 @@ const Map = memo(() => {
         /** 마커 마우스오버 이벤트 */
         kakao.maps.event.addListener(marker, "mouseover", function () {
           // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
-          infowindow.open(map, marker);
+          infowindow.open(replMap, marker);
           // 마커 이미지 약간 확대
           marker.setImage(overImage);
           document.querySelector(".loc" + i).classList.add("hover");
@@ -135,14 +99,14 @@ const Map = memo(() => {
         /** 마커 마우스클릭 이벤트 */
         kakao.maps.event.addListener(marker, "click", function () {
           // 마커에 마우스오버 이벤트가 발생하면 인포윈도우를 마커위에 표시합니다
-          infowindow.open(map, marker);
+          infowindow.open(replMap, marker);
           document.querySelector(".loc" + i).scrollIntoView({ behavior: "smooth" });
           setBtnActive(i);
         });
 
         /** 목록에 마우스엔터,리브시 마커에 인포윈도우 출력 이벤트 */
         document.querySelector(".loc" + i).addEventListener("mouseenter", (e) => {
-          infowindow.open(map, marker);
+          infowindow.open(replMap, marker);
           e.currentTarget.classList.add("hover");
           marker.setImage(overImage);
         });
@@ -157,18 +121,18 @@ const Map = memo(() => {
         document.querySelector(".loc" + i).addEventListener("click", (e) => {
           var moveLatLng = new kakao.maps.LatLng(v.lat, v.lng);
           setBtnActive(i);
-          map.panTo(moveLatLng);
+          replMap.panTo(moveLatLng);
         });
       });
 
       /**
        * 지도 클릭 위치 콘솔에 띄움
        */
-      kakao.maps.event.addListener(map, "click", function (mouseEvent) {
-        //클릭한 위도, 경도 정보를 가져옵니다.
-        const latlng = mouseEvent.latLng;
-        console.log("현재 클릭한 위치의 위도: " + latlng.getLat() + ", 경도: " + latlng.getLng());
-      });
+      // kakao.maps.event.addListener(kakaoMap, "click", function (mouseEvent) {
+      //   //클릭한 위도, 경도 정보를 가져옵니다.
+      //   const latlng = mouseEvent.latLng;
+      //   console.log("현재 클릭한 위치의 위도: " + latlng.getLat() + ", 경도: " + latlng.getLng());
+      // });
 
       /**
        * 현재 위치 찾기
@@ -203,7 +167,7 @@ const Map = memo(() => {
 
         // 마커를 생성합니다
         var marker = new kakao.maps.Marker({
-          map: map,
+          map: replMap,
           position: locPosition,
           image: markerImage, // 마커 이미지
         });
@@ -218,29 +182,37 @@ const Map = memo(() => {
         });
 
         // 인포윈도우를 마커위에 표시합니다
-        infowindow.open(map, marker);
+        infowindow.open(replMap, marker);
 
         // 지도 중심좌표를 접속위치로 변경합니다
-        map.setCenter(locPosition);
+        replMap.setCenter(locPosition);
       }
     }
-  }, [data]);
+  }, [data, swLimit, neLimit]);
 
   const onSearchLoc = useCallback((e) => {
     // 지도의 현재 중심좌표를 얻어옵니다
-    var center = kakaoMap.getCenter();
+    var center = replMap.getCenter();
 
     // 지도의 현재 영역을 얻어옵니다
-    var bounds = kakaoMap.getBounds();
+    var bounds = replMap.getBounds();
     // 영역의 남서쪽 좌표를 얻어옵니다
     var swLatLng = bounds.getSouthWest();
     // 영역의 북동쪽 좌표를 얻어옵니다
     var neLatLng = bounds.getNorthEast();
 
-    console.log(swLatLng);
-    console.log(neLatLng);
+    console.log("현재 영역의 남서쪽 좌표 : " + swLatLng + ", 북동쪽 좌표 : " + neLatLng);
 
-    console.log(data);
+    setSwLimit(swLatLng);
+    setNeLimit(neLatLng);
+
+    setCenterCoord([center["Ma"], center["La"]]);
+  });
+
+  const onModalIsOpen = useCallback((e) => {
+    setModalContent(e.currentTarget.dataset.id);
+    setModalIsOpen(true);
+    console.log(data.modalContent);
   });
 
   return (
@@ -261,13 +233,87 @@ const Map = memo(() => {
       <ListContainer id="container">
         {data?.map((v, i) => {
           return (
-            <div key={i} data-loc={v.latlng} data-title={v.title} className={`${"loc" + i} ${i == btnActive ? "active" : ""}`}>
+            <div key={i} data-loc={v.latlng} data-title={v.title} className={`${"list_item"} ${"loc" + i} ${i == btnActive ? "active" : ""}`}>
               <h3>{v.title}</h3>
               <h4>{v.address}</h4>
-              <h4>🧑‍💻</h4>
+              <a>🧑‍💻 혼자 노트북들고 작업하러 가기 좋은 곳</a>
+              <div className="more_btn" onClick={onModalIsOpen} data-id={v.id}>
+                <img src={iconMore} />
+              </div>
             </div>
           );
         })}
+
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={() => setModalIsOpen(false)}
+          ariaHideApp={false}
+          style={{
+            overlay: {
+              backgroundColor: "rgba(50, 50, 50, 0.75)",
+              zIndex: 99999,
+            },
+            content: {
+              backgroundColor: "#F8F8F8",
+              width: "700px",
+              maxHeight: "1000px",
+              left: "300px",
+              borderRadius: "15px",
+              padding: "40px",
+              margin: "auto",
+            },
+          }}>
+          {data?.map((v, i) => {
+            if (v["id"] == modalContent)
+              return (
+                <ModalContainer>
+                  <div className="modal-header">
+                    <h3>{v["title"]}</h3>
+                    <span>{v["address"]}</span>
+                    <FontAwesomeIcon className="faX" icon={faX} onClick={() => setModalIsOpen(false)} />
+                  </div>
+                  <div className="modal-body">
+                    <div className="modal-img-container">
+                      <div className="icon">
+                        <img src={iconPlus} />
+                      </div>
+                      <div className="text">
+                        여기를 눌러서 장소와 관련된 사진을 올려주시면
+                        <br />
+                        페이지가 더 유익해 질 것 같아요!
+                      </div>
+                    </div>
+                    <div className="modal-info-container">
+                      <div className="info">
+                        <div className="info-item">
+                          <div className="title">여기는 어떤 곳인가요?</div>
+                          <div className="theme-card">🛋 편안한 의자가 있어서 책 읽기 좋은 카페</div>
+                          <div className="theme-card">💻 혼자 노트북들고 작업하러 가기 좋은 곳</div>
+                        </div>
+                        <div className="info-item">
+                          <div className="title">123</div>
+                        </div>
+                      </div>
+                      <div className="info">
+                        <div className="info-item">
+                          <div className="title">이 장소에 대한 후기들!</div>
+                          <div className="theme-card">☕️두유 옵션 제공 카페</div>
+                          <div className="theme-card">🛋 편안한 의자가 있어서 책 읽기 좋은 카페</div>
+                        </div>
+                        <div className="info-item">
+                          <div className="title">직접 의견을 남겨보세요!</div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="modal-bullet-container">
+                      <div className="title">이 장소를 추천한 게시글 목록</div>
+                    </div>
+                  </div>
+                  <div className="modal-footer"></div>
+                </ModalContainer>
+              );
+          })}
+        </Modal>
       </ListContainer>
     </MapContainer>
   );
---
+++ src/components/map/MapStyled.js
@@ -0,0 +1,254 @@
+import styled from "styled-components";
+
+/** MapContainer */
+export const MapContainer = styled.div`
+  .yourLoc {
+    font-size: 50px;
+    color: red;
+    position: fixed;
+    right: 4vw;
+    bottom: 4vw;
+    z-index: 1;
+    background-color: white;
+    padding: 10px;
+    border-radius: 100%;
+    cursor: pointer;
+    opacity: 0.8;
+    box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+`;
+
+/** ListContainer */
+export const ListContainer = styled.div`
+  position: fixed;
+  height: 100%;
+  top: 70px;
+  left: 20px;
+  z-index: 1;
+  overflow-y: scroll;
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+
+  &::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Opera*/
+  }
+
+  .list_item {
+    background-color: rgb(255, 255, 255, 0.99);
+    border-radius: 10px;
+    width: 290px;
+    height: 110px;
+    margin: 10px;
+    padding: 15px 20px;
+    box-sizing: border-box;
+    cursor: pointer;
+    transition: all 0.2s;
+    box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 10px, rgba(0, 0, 0, 0.23) 0px 6px 6px;
+    letter-spacing: -0.5px;
+    position: relative;
+
+    .more_btn {
+      position: absolute;
+      top: 8px;
+      right: 7px;
+      padding: 7px 11px 4px;
+      box-sizing: border-box;
+      line-height: 32.3px;
+
+      &:hover {
+        border-radius: 5px;
+        background-color: #80b3e6;
+      }
+
+      img {
+        height: 16px;
+      }
+    }
+
+    h3 {
+      font-weight: 600;
+      color: #0581bb;
+      font-size: 16px;
+      line-height: 30.4px;
+    }
+
+    h4 {
+      font-size: 11px;
+      margin-bottom: 12px;
+      color: #adadad;
+      font-weight: 400;
+      line-height: 20.9px;
+    }
+
+    a {
+      font-size: 12px;
+      color: #545454;
+      font-weight: 200;
+    }
+
+    &.hover {
+      background-color: #ccc;
+      color: white;
+
+      h3 {
+        color: white;
+      }
+
+      h4 {
+        color: #fff;
+      }
+    }
+
+    &.active {
+      background-color: #39f;
+      color: white;
+
+      h3 {
+        color: white;
+      }
+
+      h4 {
+        color: #fff;
+      }
+    }
+  }
+
+  .modal-bg {
+    width: 100vw;
+    height: 100vh;
+    /* background-color: red; */
+    position: fixed;
+    top: 0;
+  }
+`;
+
+export const SearchLoc = styled.div`
+  width: 200px;
+  height: 50px;
+  border-radius: 10px;
+  background-color: #da4c1f;
+  z-index: 1;
+  position: fixed;
+  line-height: 1.8;
+  font-weight: bold;
+  color: white;
+  bottom: 4vw;
+  left: 50%;
+  transform: translate(-50%, 0);
+  opacity: 0.8;
+  padding: 10px;
+  box-sizing: border-box;
+  text-align: center;
+  cursor: pointer;
+  display: flex;
+
+  &:hover {
+    opacity: 1;
+  }
+
+  span {
+    width: 80%;
+  }
+
+  input,
+  svg {
+    width: 20%;
+    margin: 6.5px 0;
+  }
+`;
+
+export const ModalContainer = styled.div`
+  letter-spacing: -0.5px;
+  color: #666666;
+  line-height: 21.45px;
+
+  .modal-header {
+    padding: 0 0 30px;
+    position: relative;
+    h3 {
+      color: #0581bb;
+      font-weight: 600;
+      font-size: 21px;
+      margin-bottom: 8px;
+    }
+
+    span {
+      font-size: 13px;
+      cursor: pointer;
+
+      &:hover {
+        color: black;
+      }
+    }
+
+    .faX {
+      font-size: 30px;
+      position: absolute;
+      right: 0;
+      top: 0;
+      cursor: pointer;
+    }
+  }
+
+  .modal-body {
+    .title {
+      font-size: 14px;
+      font-weight: 600;
+    }
+    .modal-img-container {
+      width: 700px;
+      height: 160px;
+      background-color: #e5e5e5;
+      text-align: center;
+      display: flex;
+      align-items: center;
+      flex-direction: column;
+      justify-content: center;
+      cursor: pointer;
+      margin-bottom: 30px;
+
+      .icon {
+        margin-bottom: 10px;
+      }
+
+      .text {
+        font-size: 13px;
+        font-weight: 400;
+        line-height: 21.45px;
+      }
+    }
+
+    .modal-info-container {
+      display: flex;
+
+      .info {
+        width: 50%;
+        padding-right: 20px;
+
+        &:last-of-type {
+          padding-right: 0;
+          padding-left: 20px;
+        }
+
+        .info-item {
+          margin-bottom: 40px;
+
+          .theme-card {
+            margin: 8px 0;
+            font-size: 13px;
+            width: 330px;
+            height: 51.5px;
+            padding: 15px 20px;
+            box-sizing: border-box;
+            box-shadow: 3px 3px 8px rgb(0 0 0 / 20%);
+            border-radius: 12px;
+          }
+        }
+      }
+    }
+  }
+`;
