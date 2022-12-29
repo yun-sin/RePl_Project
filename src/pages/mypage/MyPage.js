7bcb0d760d244b12b1761253ad61272c3da7d43c
zzemoo <chaerim.dev@gmail.com>
2022-12-29 19:10:08 +0900

    마이페이지 탭 수정

--- src/pages/mypage/MyPage.js
+++ src/pages/mypage/MyPage.js
@@ -17,24 +17,71 @@ const MenuTab = styled.nav`
     flex-direction: column;
 `
 
+const TabMenu = styled.ul`
+
+    font-size: 20px;
+    cursor: pointer;
+    text-decoration: none;
+    padding: 10px;
+    color: #222;
+    border: 1px solid #D2D1D1;
+    border-radius: 0 50px 50px 0;
+    box-sizing: border-box;
+    font-size: 17px;
+    width: 200px;
+    margin-bottom: 10px;
+    text-align: center;
+    
+    &:hover {
+        color: #0584BB;
+    }
+
+    &.focused {
+        color: white;
+        background-color: #0584BB;
+        border-color: #0584BB;
+
+        &:after {
+            border-bottom: 4px solid #fff !important;
+        }
+    }
+`;
+
 const MyPage = memo(() => {
+
+    const [currentTab, setCurrentTab] = React.useState(0);
+
+    const menuArr = [
+        '내 정보관리',
+        '내 관심사',
+        '나의 지도 관리',
+        '팔로워/팔로잉',
+        '신고 목록',
+      ];
+    
+      const selectMenuHandler = (index) => {
+        setCurrentTab(index);
+      };
     return (
         <div>
             <MenuTab>
-                <MenuLink to="info">내 정보관리</MenuLink>
-                <MenuLink to="interests">내 관심사</MenuLink>
-                <MenuLink to="map">나의 지도 관리</MenuLink>
-                <MenuLink to="follow">팔로워/팔로잉</MenuLink>
-                <MenuLink to="noti">신고 목록</MenuLink>
+                {menuArr.map((v, index)=>{
+                    return (
+                    <TabMenu
+                        key={index}
+                        className={currentTab === index ? "submenu focused" : "submenu"}
+                        onClick={()=> selectMenuHandler(index)}
+                        >
+                    {v}
+                    </TabMenu>
+                    )
+                })}
             </MenuTab>
-
-            <Routes>
-                <Route path="info" element={<Info/>} />
-                <Route path="interests" element={<Interests/>} />
-                <Route path="map" element={<Map/>} />
-                <Route path="follow" element={<Follow/>} />
-                <Route path="noti" element={<Noti/>} />
-            </Routes>
+            {currentTab === 0 && <Info/>}
+            {currentTab === 1 && <Interests/>}
+            {currentTab === 2 && <Map/>}
+            {currentTab === 3 && <Follow/>}
+            {currentTab === 4 && <Noti/>}
         </div>
     );
 });
