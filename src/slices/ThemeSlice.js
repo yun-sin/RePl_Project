203c64ce6bce9789906c0659c82e761e44ecc9bf
sqhtjs0104 <sqhtjs0104@gmail.com>
2023-01-26 19:43:19 +0900

    카테고리 데이터형태 수정

--- src/slices/ThemeSlice.js
+++ src/slices/ThemeSlice.js
@@ -29,13 +29,14 @@ export const getThemeData = createAsyncThunk(
       result = rejectWithValue(err.response);
     }
 
+    console.log(result);
+
     if (result.item) {
       return result.item
     } else {
       return result;
 
     }
-    
   }
 );
 
